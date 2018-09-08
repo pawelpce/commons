006c27d1d3293c1ebf5932ff35c12b37707e9018
PiotrKotynia <alkor88@o2.pl>
2018-09-08 11:05:30 +0200

    commented validation in order to be in line with sql fill-in file

---
+++ src/main/java/pl/sda/commons/entities/Car.java
@@ -0,0 +1,66 @@
+package pl.sda.commons.entities;
+
+import javax.validation.constraints.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "Car")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "carID")
+    private Integer carID;
+
+//    @NotNull
+//    @Size(min = 3, max = 10)
+    @Column(name = "color")
+    private String color;
+
+//    @NotNull
+//    @Size(min = 2)
+    @Column(name = "type")
+    private String type;
+
+//    @NotNull
+//    @Size(min = 2)
+    @Column(name = "mark")
+    private String mark;
+
+//    @NotNull
+    @Column(name = "year")
+    private Integer year;
+
+//    @NotNull
+//    @Size(min = 10, max = 20)
+    @Column(name = "VIN")
+    private String VIN;
+
+    @ManyToOne
+    @JoinColumn(name = "clientID")
+    private Client client;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "cars", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Worker> workers;
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "carID=" + carID +
+                ", color='" + color + '\'' +
+                ", type='" + type + '\'' +
+                ", mark='" + mark + '\'' +
+                '}';
+    }
+}
